f7eb9543197a7c576c95e0809e707ee6524af687
BishopH7 <77747379+BishopH7@users.noreply.github.com>
2021-04-15 09:36:20 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,11 @@
+from flask import Flask, request, Markup, render_template, flash, Markup
+import os
+import json
 
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+    print("RunningMain")
+    with open('cancer.json') as cancer_data:
+        states = json.load(cancer_data)
