2767580cce761a193488e544178fd406bd09cc34
BishopH7 <77747379+BishopH7@users.noreply.github.com>
2021-04-15 10:05:49 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,3 +9,18 @@ def render_main():
     print("RunningMain")
     with open('cancer.json') as cancer_data:
         states = json.load(cancer_data)
+        
+        if 'states' in request.args:
+        return render_template('page2.html', states = get_state_options(states), total_rate = total_rate(request.args['states']))
+
+    def get_state_options(states):
+    states = []
+    print("RunningOP")
+    for data in states:
+        if data["State"] not in states:
+            states.append(data["State"])
+    options = ""
+    for data in states:
+        options = options + Markup("<option value=\"" + data + "\">" + data + "</option>")
+    return options
+
