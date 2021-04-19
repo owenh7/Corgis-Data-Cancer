267c396a906bff8e41084c6bcab48791f7f05803
BishopH7 <77747379+BishopH7@users.noreply.github.com>
2021-04-19 09:07:48 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -23,10 +23,10 @@ def render_first3():
 
 
         
-if 'states' in request.args:
-    return render_template('page2.html', states = get_state_options(state))
-elif 'states' not in request.args:
-    return render_template('page2.html', states = get_state_options(state))
+    if 'states' in request.args:
+        return render_template('page2.html', states = get_state_options(state))
+    elif 'states' not in request.args:
+        return render_template('page2.html', states = get_state_options(state))
 
 def get_state_options(state):
     states = []
