bc66d4129de63908b92d34dba00ab1010313dc32
BishopH7 <77747379+BishopH7@users.noreply.github.com>
2021-06-01 17:51:07 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -20,7 +20,36 @@ def render_first2():
 @app.route("/p3")
 def render_first3():
     return render_template('page3.html')
+with open('cancer.json') as cancer_data:
+        counties = json.load(cancer_data)
+    if 'counties' in request.args:
+        return render_template('page2.html', states = get_state_options(counties), Total_Rate = Total_Rate(get_issued_state(request.args['counties'],counties), counties), counties = get_issued_options(get_issued_state(request.args['counties'],counties),counties), issued_awarded = get_issued_awarded(request.args['counties'],counties))
+    if 'states' in request.args:
+        return render_template('page2.html', states = get_state_options(counties), Total_Rate = Total_Rate(request.args['states'], counties), counties = get_issued_options(request.args['states'],counties))
+    elif 'states' not in request.args and 'counties' not in request.args:
+        return render_template('page2.html', states = get_state_options(counties))
+    
 
+def get_state_options(counties):
+    states = []
+    print("RunningOP")
+    for data in counties:
+        if data["State"] not in states:
+            states.append(data["State"])
+    options = ""
+    for data in states:
+        options = options + Markup("<option value=\"" + str(data) + "\">" + str(data) + "</option>")
+    return options
+
+def Total_Rate(state, counties):
+    print("RunningAge")
+    points = float(0)
+    total = float(0)
+    for issued in counties:
+        if issued["State"] == state:
+            total = issued["Total"]["Rate"]
+    return total
+    return render_template('page2.html')
 
         
   
